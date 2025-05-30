4a3c344ff98aacdc06fa0fc849e270c17018e078
luqmanthinkpad <luqmanthinkpad@gmail.com>
2025-05-30 08:33:17 +0700

    Create social.js

---
+++ social.js
@@ -0,0 +1,12 @@
+document.write(
+'<div id="fixedban" style="width:100%;margin:auto;text-align:center;float:none;overflow:hidden;display:scroll;position:fixed;bottom:0;z-index:999;-webkit-transform:translateZ(0);"><div><a id="close-fixedban" onclick="document.getElementById(&quot;fixedban&quot;).style.display = &quot;none&quot;;" style="cursor:pointer;"><img alt="close" src="https://1.bp.blogspot.com/-KlqIfiFJtf4/YUZuapc3EQI/AAAAAAAAAHk/eJ6C7ejCU44J4AeNO_9Ka8PSO9PFV38zwCNcBGAsYHQ/s24/cancel.png" title="close button" style="vertical-align:middle;" /></a></div><div style="text-align:center;display:block;max-width:728px;height:auto;overflow:hidden;margin:auto">'+
+'<scr'+'ipt type="text/javascript">'+
+	'atOptions = {'+
+		'\'key\' : \'17a7b6540a43433373edbc61f85bb1d6\','+
+		'\'format\' : \'iframe\','+
+		'\'height\' : 250,'+
+		'\'width\' : 300,'+
+		'\'params\' : {}'+
+	'};'+
+	"document.write('<scr' + 'ipt type=\"text/javascript\" src=\"http' + (location.protocol === 'https:' ? 's' : '') + '://democraticexit.com/17a7b6540a43433373edbc61f85bb1d6/invoke.js\"></scr' + 'ipt>');"+
+'</scr'+'ipt>');
