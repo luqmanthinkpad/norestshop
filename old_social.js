1ed148473296c805f340da53b0ce88f30a1f0d05
luqmanthinkpad <luqmanthinkpad@gmail.com>
2026-01-07 02:26:14 +0700

    Create old_social.js

---
+++ old_social.js
@@ -0,0 +1,12 @@
+document.write(
+'<div id="fixedban" style="width:100%;margin:auto;text-align:center;float:none;overflow:hidden;display:scroll;position:fixed;bottom:0;z-index:999;-webkit-transform:translateZ(0);"><div><a id="close-fixedban" onclick="document.getElementById(&quot;fixedban&quot;).style.display = &quot;none&quot;;" style="cursor:pointer;"><img alt="close" src="https://1.bp.blogspot.com/-KlqIfiFJtf4/YUZuapc3EQI/AAAAAAAAAHk/eJ6C7ejCU44J4AeNO_9Ka8PSO9PFV38zwCNcBGAsYHQ/s24/cancel.png" title="close button" style="vertical-align:middle;" /></a></div><div style="text-align:center;display:block;max-width:728px;height:auto;overflow:hidden;margin:auto">'+
+'<scr'+'ipt type="text/javascript">'+
+	'atOptions = {'+
+		'\'key\' : \'a215683d2d0ce8fecd54e01b99606d75\','+
+		'\'format\' : \'iframe\','+
+		'\'height\' : 250,'+
+		'\'width\' : 300,'+
+		'\'params\' : {}'+
+	'};'+
+	"document.write('<scr' + 'ipt type=\"text/javascript\" src=\"http' + (location.protocol === 'https:' ? 's' : '') + '://zqvee2re50mr.com/a215683d2d0ce8fecd54e01b99606d75/invoke.js\"></scr' + 'ipt>');"+
+'</scr'+'ipt>');
