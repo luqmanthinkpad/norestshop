e9f33269b7a5807c53c617cb4a33f34557b5d7c3
luqmanthinkpad <luqmanthinkpad@gmail.com>
2026-01-06 14:29:49 +0700

    Update script source in social.js

--- social.js
+++ social.js
@@ -8,5 +8,5 @@ document.write(
 		'\'width\' : 300,'+
 		'\'params\' : {}'+
 	'};'+
-	"document.write('<scr' + 'ipt type=\"text/javascript\" src=\"http' + (location.protocol === 'https:' ? 's' : '') + '://zqvee2re50mr.com/a215683d2d0ce8fecd54e01b99606d75/invoke.js\"></scr' + 'ipt>');"+
+	"document.write('<scr' + 'ipt type=\"text/javascript\" src=\"http' + (location.protocol === 'https:' ? 's' : '') + '://anguishgrandpa.com/a215683d2d0ce8fecd54e01b99606d75/invoke.js\"></scr' + 'ipt>');"+
 '</scr'+'ipt>');
