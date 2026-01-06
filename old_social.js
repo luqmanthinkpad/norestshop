2994f81410ee96b3694a0d689369ba51344a8abf
luqmanthinkpad <luqmanthinkpad@gmail.com>
2026-01-07 02:39:05 +0700

    Update old_social.js

--- old_social.js
+++ old_social.js
@@ -1,5 +1,5 @@
 document.write(
-'<div id="fixedban" style="width:100%;margin:auto;text-align:center;float:none;overflow:hidden;display:scroll;position:fixed;bottom:0;z-index:999;-webkit-transform:translateZ(0);"><div><a id="close-fixedban" onclick="document.getElementById(&quot;fixedban&quot;).style.display = &quot;none&quot;;" style="cursor:pointer;"><img alt="close" src="https://1.bp.blogspot.com/-KlqIfiFJtf4/YUZuapc3EQI/AAAAAAAAAHk/eJ6C7ejCU44J4AeNO_9Ka8PSO9PFV38zwCNcBGAsYHQ/s24/cancel.png" title="close button" style="vertical-align:middle;" /></a></div><div style="text-align:center;display:block;max-width:728px;height:auto;overflow:hidden;margin:auto">'+
+'<div id="fixedban" style="width:100%;margin:auto;text-align:center;float:none;overflow:hidden;display:scroll;position:fixed;bottom:0;z-index:999;-webkit-transform:translateZ(0);"><div style="text-align:center;display:block;max-width:728px;height:auto;overflow:hidden;margin:auto">'+
 '<scr'+'ipt type="text/javascript">'+
 	'atOptions = {'+
 		'\'key\' : \'a215683d2d0ce8fecd54e01b99606d75\','+
